e8177d26e4272d5702727542f8acc63d0d68c80f
TristanVDK <calgara@me.com>
2026-06-30 11:00:23 +0200

    Page "SignIn.jsx" fonctionnelle, ajout de la logique de connexion.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,49 @@
+import React from 'react';
+import { useState } from 'react';
+// import './SignIn.scss';
+
+
 function SignIn() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Identifiants saisis :", username, password)
+    }
+
     return (
-        <main>
-            <h1>Page de login</h1>
+        <main className="main bg-dark">
+            <section className="sign-in-content">
+                <i className="fa fa-user-circle sign-in-icon"></i>
+                <h1>Sign In</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-wrapper">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            id="username"
+                            value={username}
+                            onChange={(event) => setUsername(event.target.value)}
+                        />
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                            />
+                    </div>
+                    <div className="input-remember">
+                        <input type="checkbox" id="remember-me" />
+                        <label htmlFor="remember-me">Remember me</label>
+                    </div>
+                    <button type="submit" className="sign-in-button">Sign In</button>
+                </form>
+            </section>
         </main>
     );
 }
